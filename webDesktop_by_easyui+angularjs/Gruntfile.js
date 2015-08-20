a95cd00c13d32503d6cd1d6cccff960e1adc58aa
scott <mic1028002@gmail.com>
2015-08-20 08:52:04 +0800

    start a easyui version webDesktop Scaffold

---
+++ webDesktop_by_easyui+angularjs/Gruntfile.js
@@ -0,0 +1,69 @@
+'use strict';
+
+module.exports = function(grunt) {
+    
+
+    //
+    var staticFileMiddleware = function(req, res, next) {
+        var finalhandler = require('finalhandler');  // if you no use this plugin, when client get nonexisted file, connect server will shutdown.
+        var serveStatic = require('serve-static');
+        var serve = serveStatic(staticPath);
+        var done = finalhandler(req, res);
+        serve(req, res, done);
+    };
+
+
+    //
+    var allMiddlewares = function(connect, options) {
+        var optBase = (typeof options.base === 'string') ? [options.base] : options.base;
+        return [
+            require('connect-modrewrite')(require('./.htaccess.js').rewrite),   // rewriteRule support
+            staticFileMiddleware    // staticFile serve
+        ]
+    }
+
+
+    //
+    var staticPath = './app';
+
+
+    //
+    grunt.loadNpmTasks('grunt-wiredep');
+
+
+    //
+    grunt.initConfig({
+        wiredep: {
+            task: {
+                src: [
+                    './app/*.html', // .html support...
+                ],
+            }
+        },
+        connect: {
+            options: {
+                port: 3333,
+                hostname: '*',
+                keepalive: true,    // 維持伺服器開啟
+                debug: true
+            },
+            development: {
+                options: {
+                    middleware: allMiddlewares
+                }
+            }
+        }
+    });
+
+
+    //
+    grunt.loadNpmTasks('grunt-contrib-connect');
+
+
+    //
+    grunt.registerTask('lift', function (target) {
+        grunt.task.run([
+            'connect:development'
+        ]);
+    });
+};
