5ea2ef02989c0201455497e4bae5e90dea1f0fa4
scott.lan <scott.lan@taisys.com>
2015-08-20 16:48:58 +0800

    add comment

--- webDesktop_by_extjs4/.htaccess.js
+++ webDesktop_by_extjs4/.htaccess.js
@@ -5,6 +5,7 @@
 // default .htaccess
 module.exports = {
     rewrite: [
+        // '^/shared/(.*)$ http://cdn.sencha.com/ext/gpl/4.2.1/examples/shared/$1 [P,L]',
         '^/api/(.*)$ http://java.dev.gslssd.com/api/$1 [P,L]',
         '^/taisysdev/(.*)$ http://s3-ap-southeast-1.amazonaws.com/taisysdev/$1 [P,L]',
         '!\\.js|\\.html|\\.css|\\.png|\\.jpg|\\.gif|\\.svg|\\.ttf|\\.woff|\\.ico$ /index.html [L]'
